ded3c3132ac1fcbf1acb3905e80508008eab899c
Kainy Ryu <kainy2109@gmail.com>
2022-07-03 17:06:39 +0900

    format  files

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -8,16 +8,15 @@ export default {
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+    layout: 'fullscreen'
+  }
 } as ComponentMeta<typeof Page>;
 
-const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+const Template: ComponentStory<typeof Page> = args => <Page {...args} />;
 
 export const LoggedOut = Template.bind({});
 
 export const LoggedIn = Template.bind({});
-
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
--- storybook-static/assets/Button.stories.b513e0df.js
+++ storybook-static/assets/Button.stories.b513e0df.js
@@ -1,4 +1,23 @@
-var c=Object.defineProperty;var e=Object.getOwnPropertySymbols;var i=Object.prototype.hasOwnProperty,p=Object.prototype.propertyIsEnumerable;var a=(n,o,t)=>o in n?c(n,o,{enumerable:!0,configurable:!0,writable:!0,value:t}):n[o]=t,l=(n,o)=>{for(var t in o||(o={}))i.call(o,t)&&a(n,t,o[t]);if(e)for(var t of e(o))p.call(o,t)&&a(n,t,o[t]);return n};import{B as s}from"./Button.490d214f.js";import{j as d}from"./jsx-runtime.bd940121.js";import"./index.d1b1a1f8.js";var f={parameters:{storySource:{source:`import React from 'react';
+var c = Object.defineProperty;
+var e = Object.getOwnPropertySymbols;
+var i = Object.prototype.hasOwnProperty,
+  p = Object.prototype.propertyIsEnumerable;
+var a = (n, o, t) =>
+    o in n
+      ? c(n, o, { enumerable: !0, configurable: !0, writable: !0, value: t })
+      : (n[o] = t),
+  l = (n, o) => {
+    for (var t in o || (o = {})) i.call(o, t) && a(n, t, o[t]);
+    if (e) for (var t of e(o)) p.call(o, t) && a(n, t, o[t]);
+    return n;
+  };
+import { B as s } from './Button.490d214f.js';
+import { j as d } from './jsx-runtime.bd940121.js';
+import './index.d1b1a1f8.js';
+var f = {
+  parameters: {
+    storySource: {
+      source: `import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Button } from './Button';
@@ -39,5 +58,55 @@ Small.args = {
   size: 'small',
   label: 'Button',
 };
-`,locationsMap:{primary:{startLoc:{col:48,line:17},endLoc:{col:78,line:17},startBody:{col:48,line:17},endBody:{col:78,line:17}},secondary:{startLoc:{col:48,line:17},endLoc:{col:78,line:17},startBody:{col:48,line:17},endBody:{col:78,line:17}},large:{startLoc:{col:48,line:17},endLoc:{col:78,line:17},startBody:{col:48,line:17},endBody:{col:78,line:17}},small:{startLoc:{col:48,line:17},endLoc:{col:78,line:17},startBody:{col:48,line:17},endBody:{col:78,line:17}}}}},title:"Example/Button",component:s,argTypes:{backgroundColor:{control:"color"}}};const r=n=>d(s,l({},n)),m=r.bind({});m.args={primary:!0,label:"Button"};const g=r.bind({});g.args={label:"Button"};const y=r.bind({});y.args={size:"large",label:"Button"};const u=r.bind({});u.args={size:"small",label:"Button"};const S=["Primary","Secondary","Large","Small"];export{y as Large,m as Primary,g as Secondary,u as Small,S as __namedExportsOrder,f as default};
+`,
+      locationsMap: {
+        primary: {
+          startLoc: { col: 48, line: 17 },
+          endLoc: { col: 78, line: 17 },
+          startBody: { col: 48, line: 17 },
+          endBody: { col: 78, line: 17 }
+        },
+        secondary: {
+          startLoc: { col: 48, line: 17 },
+          endLoc: { col: 78, line: 17 },
+          startBody: { col: 48, line: 17 },
+          endBody: { col: 78, line: 17 }
+        },
+        large: {
+          startLoc: { col: 48, line: 17 },
+          endLoc: { col: 78, line: 17 },
+          startBody: { col: 48, line: 17 },
+          endBody: { col: 78, line: 17 }
+        },
+        small: {
+          startLoc: { col: 48, line: 17 },
+          endLoc: { col: 78, line: 17 },
+          startBody: { col: 48, line: 17 },
+          endBody: { col: 78, line: 17 }
+        }
+      }
+    }
+  },
+  title: 'Example/Button',
+  component: s,
+  argTypes: { backgroundColor: { control: 'color' } }
+};
+const r = n => d(s, l({}, n)),
+  m = r.bind({});
+m.args = { primary: !0, label: 'Button' };
+const g = r.bind({});
+g.args = { label: 'Button' };
+const y = r.bind({});
+y.args = { size: 'large', label: 'Button' };
+const u = r.bind({});
+u.args = { size: 'small', label: 'Button' };
+const S = ['Primary', 'Secondary', 'Large', 'Small'];
+export {
+  y as Large,
+  m as Primary,
+  g as Secondary,
+  u as Small,
+  S as __namedExportsOrder,
+  f as default
+};
 //# sourceMappingURL=Button.stories.b513e0df.js.map
